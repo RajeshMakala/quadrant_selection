44cf5f063450d71818fd24399bd3657f043552db
rajeshmakala <rajeshmakala@SURFACE>
2021-06-01 14:26:15 -0400

    Add project files.

---
+++ quadrant_selection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace quadrant_selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("4");
+            }
+
+        }
+    }
+}
